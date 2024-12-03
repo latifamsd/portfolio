61a63e1dc27614a7f3cb19fb119d3b3f734656b7
latifa moussaid <titifamoussaid@gmail.com>
2024-12-03 12:19:24 +0100

    add

--- script.js
+++ script.js
@@ -165,3 +165,24 @@ scrollToTopBtn.addEventListener('click', () => {
         behavior: 'smooth' // Défilement fluide
     });
 });
+$(document).ready(function() {
+  $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 4 }
+      },
+      {
+        breakpoint: 520,
+        settings: { slidesToShow: 3 }
+      }
+    ]
+  });
+});
